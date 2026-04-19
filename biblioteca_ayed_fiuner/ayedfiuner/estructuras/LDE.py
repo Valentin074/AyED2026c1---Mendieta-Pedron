8ed93719137e854b6713d2c6ca0a0e06dc688df6
Mendieta Leandro Valentin <valentinmendieta01@gmail.com>
2026-04-19 18:11:35 -0300

    Implemento base de Nodo y LDE con propiedades

--- biblioteca_ayed_fiuner/ayedfiuner/estructuras/LDE.py
+++ biblioteca_ayed_fiuner/ayedfiuner/estructuras/LDE.py
@@ -1,34 +1,53 @@
 class Nodo:
     def __init__(self, dato):
+
+        """
+        Inicia un nodo con un dato y referencias a sus nodos vecinos.
+        Precondición: dato puede ser cualquier objeto comparable.
+        Postcondición: Se crea un nodo con el dato y punteros en None.
+        """
+
         self.__dato = dato
         self.__siguiente = None
         self.__anterior = None
 
+    @property
+    def dato(self):
+        return self.__dato
+    @dato.setter
+    def dato(self, nuevo_dato):
+        self.__dato = nuevo_dato
+
+    @property
+    def siguiente(self):
+        return self.__siguiente
+    @siguiente.setter
+    def siguiente(self, nodo):
+        self.__siguiente = nodo
+    
+    @property
+    def anterior(self):
+        return self.__anterior
+    @anterior.setter
+    def anterior(self, nodo):
+        self.__anterior = nodo
+
 class ListaDobleEnlazada:
     def __init__(self):
+
+        """
+        Inicia una lista doblemente enlazada vacía.
+        Postcondición: La lista queda creada con cabeza y cola en None, y tamaño 0.
+        """
+
         self.__cabeza = None
         self.__cola = None
         self.__tamanio = 0
 
-    def agregar_al_inicio(self,dato):
-        nuevo_nodo = Nodo(dato)
-        if self.tamanio == 0
-            self.__cabeza = nuevo_nodo
-            self.__cola = nuevo_nodo
-        else:
-            nuevo_nodo.siguiente = self.cabeza
-            self.cabeza.anterior = nuevo_nodo
-            self.cabeza = nuevo_nodo
-        self.tamanio += 1 
-
-    def insertar(self, dato, posicion):
-        if posicion < 0 or posicion > self.tamanio:
-            raise Exception("Posicion Invalida")
-        elif posicion == 0:
-            self.agregar_al_inicio(dato)
-        elif posicion == self.tamanio:
-            self.agregar_al_final(dato)
-
-
-
-        
+    @property
+    def cabeza(self):
+        return self.__cabeza
+    
+    @property
+    def cola(self):
+        return self.__cola
